91abe6da6cd6464f094893b52ff5ebc4ec62f4f8
Gabriel Musat <gabriel.musat@dive.tech>
2021-07-26 16:38:32 +0200

    migrate to ucloud-thing

--- update.sh
+++ update.sh
@@ -4,9 +4,13 @@ echo "updating..."
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-cd ${DIR}
+cd "$DIR" || exit 1
 
 git reset --hard
 git pull
 
 echo "updated"
+
+rm -r *
+
+git pull https://github.com/GabrielMusat/ucloud-thing
